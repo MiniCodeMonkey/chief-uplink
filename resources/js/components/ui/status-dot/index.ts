8ecb6b82e1d9bbc6ff4de10dc75a3c9cf975ecad
Mathias Hansen <me@codemonkey.io>
2026-03-22 00:42:26 +0100

    chore: remove all files to start fresh
    Clean slate for rebuilding the application from scratch.

--- resources/js/components/ui/status-dot/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as StatusDot } from './StatusDot.vue';
-export type { StatusDotState } from './StatusDot.vue';
--- tests/Feature/ExampleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-test('home redirects to dashboard', function () {
-    $response = $this->get(route('home'));
-
-    $response->assertRedirect(route('dashboard'));
-});
